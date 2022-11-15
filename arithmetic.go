2a3e23edb1f53e6445f7fb180705bd71a94b9bab
neohsoonsien <neohsbrother@gmail.com>
2022-11-15 22:47:13 +0800

    init(arithmetic): perform simple arithmetic operations and for loop

---
+++ arithmetic.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func main() {
+	var a int = 2
+	a += 2
+	fmt.Println(a)
+
+	a -= 2
+	fmt.Println(a)
+
+	b := big.NewInt(12)
+	b = b.Add(b, big.NewInt(5))
+	fmt.Println(b)
+
+	list := [4]string{"one", "two", "three", "four"}
+	for i, item := range list {
+		fmt.Println(i, item)
+	}
+}
