3617a66c167a76a145a455dc69ad78f791fcf128
neohsoonsien <neohsbrother@gmail.com>
2022-11-23 21:47:25 +0800

    update(mongodb.go): find from the collection and parse into array

--- mongodb.go
+++ mongodb.go
@@ -2,53 +2,90 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type TransactionEvent struct {
+	EventId         primitive.ObjectID `bson:"_id"`
+	BlockNumber     uint64             `json:"blockNumber"`
+	TxHash          string             `json:"transactionHash" gencodec:"required"`
+	Time            uint64             `json:"time"`
+	Sender          string             `json:"sender"`
+	Receiver        string             `json:"receiver"`
+	Value           string             `json:"value"`
+	MethodName      string             `json:"methodname"`
+	MethodParams    map[string]string  `json:"methodparams"`
+	Events          []Event            `json:"events"`
+	Token           string             `json:"token"`
+	TokenId         string             `json:"tokenid"`
+	Wallets         []string           `json:"wallets"`
+	CreatedTime     int64              `json:"CreatedTime"`
+	TransactionType string             `json:"transactiontype"`
+}
+
+type Event struct {
+	EventName   string
+	EventParams map[string]string
+	Contract    string
+}
+
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
-	}
 
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb+srv://username:password@project.ls4g1.mongodb.net/events?retryWrites=true&w=majority"))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
 	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+		if err = client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
 		}
 	}()
 
-	coll := client.Database("transactions").Collection("transactions")
-	fromAddress := "0x55238bb985B300"
+	collection := client.Database("events").Collection("events")
 
-	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"fromaddress", fromAddress}}).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the fromaddress %s\n", fromAddress)
-		return
-	}
+	// find all documents in which the "sender" field is "0xwalletaddress"
+	// specify the Sort option to sort the returned documents by age in ascending order
+	// opts := options.Find().SetSort(bson.D{{"createdtime", 1}})
+	cursor, err := collection.Find(context.TODO(), bson.D{{"methodname", "transfer"}})
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	jsonData, err := json.MarshalIndent(result, "", "    ")
-	if err != nil {
-		panic(err)
+	// doc, err := bson.Marshal(bson.D{{"methodname", "transfer"}, {"createdtime", 1}})
+
+	// get a list of all returned documents and print them out
+	// see the mongo.Cursor documentation for more examples of using cursors
+	var results []TransactionEvent
+	if err = cursor.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", jsonData)
+
+	var numSenderDiff uint = 0
+	var numReceiverDiff uint = 0
+	for _, event := range results {
+		var sender string = event.Sender
+		var receiver string = event.Receiver
+		var from string = event.Events[0].EventParams["from"]
+		var to string = event.Events[0].EventParams["to"]
+
+		if sender != from {
+			numSenderDiff++
+
+		} else if receiver != to {
+			numReceiverDiff++
+		}
+	}
+	fmt.Println("There are a total number of ", len(results), "LCUSD transactions.")
+	fmt.Println("The sender and event sender are different: ", numSenderDiff)
+	fmt.Println("The receiver and event to are different: ", numReceiverDiff)
+
+	// var test TransactionEvent
+	// err = bson.Unmarshal(doc, &test)
+	// fmt.Printf("Unmarshalled Struct:\n%+v\n", test)
 }
