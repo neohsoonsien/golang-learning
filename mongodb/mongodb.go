a088f2542e8f781d245f37f93e9b08a0863318a1
neohsoonsien <neohsbrother@gmail.com>
2022-12-30 20:49:31 +0800

    update(mongodb): read data, append bson.D data type into slice, and InsertMany into the MongoDB

--- mongodb/mongodb.go
+++ mongodb/mongodb.go
@@ -1,15 +1,23 @@
 package main
-
 import (
 	"context"
 	"fmt"
-	"log"
+	"os"
+
+	godotenv "github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type Event struct {
+	EventName   string
+	EventParams map[string]string
+	Contract    string
+}
 
 type TransactionEvent struct {
 	EventId         primitive.ObjectID `bson:"_id"`
@@ -29,70 +37,76 @@ type TransactionEvent struct {
 	TransactionType string             `json:"transactiontype"`
 }
 
-type Event struct {
-	EventName   string
-	EventParams map[string]string
-	Contract    string
-}
-
 func main() {
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://root:1qazZAQ!@localhost:27017/events"))
+	// ******************************************************* //
+	// Step 1: obtain the mongoURI connection string from .env file
+	// ******************************************************* //
+	godotenv.Load()
+	mongoURI := os.Getenv("MONGODB_URI")
+
+	// ******************************************************* //
+	// Step 2: connect to the mongodb
+	// ******************************************************* //
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer func() {
 		if err = client.Disconnect(context.TODO()); err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
 
-	collection := client.Database("events").Collection("events")
+	
+	// ******************************************************* //
+	// Step 3: finding methodname: mintWithURI from the mongodb
+	// ******************************************************* //
+	coll := client.Database("events").Collection("events")
+	filter := bson.D{{"methodname", "mintWithURI"}}
 
-	// find all documents in which the "sender" field is "0xwalletaddress"
-	// specify the Sort option to sort the returned documents by age in ascending order
-	// opts := options.Find().SetSort(bson.D{{"createdtime", 1}})
-	cursor, err := collection.Find(context.TODO(), bson.D{{"methodname", "transfer"}})
+	cursor, err := coll.Find(context.TODO(), filter)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	// doc, err := bson.Marshal(bson.D{{"methodname", "transfer"}, {"createdtime", 1}})
-
-	// get a list of all returned documents and print them out
-	// see the mongo.Cursor documentation for more examples of using cursors
 	var results []TransactionEvent
 	if err = cursor.All(context.TODO(), &results); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	var numSenderDiff uint = 0
-	var numReceiverDiff uint = 0
-	var numValueDiff uint = 0
-	var eventSizeNotFulfill uint = 0
-	for _, event := range results {
-		var sender string = event.Sender
-		var receiver string = event.MethodParams["recipient"]
-		var amount string = event.Value
-		var from string = event.Events[0].EventParams["from"]
-		var to string = event.Events[0].EventParams["to"]
-		var eventValue string = event.Events[0].EventParams["value"]
+	fmt.Println(len(results))
 
-		if sender != from {
-			numSenderDiff++
-		} else if receiver != to {
-			numReceiverDiff++
-		} else if amount != eventValue {
-			numValueDiff++
-		}
-
-		if len(event.Events) != 1 {
-			eventSizeNotFulfill++
-		}
+	// ******************************************************* //
+	// Step 4: process the results into docs, 
+	//		   which will be inserted into new collection "nfts"
+	// ******************************************************* //
+	docs := []interface{}{}
+	for _, result := range results {
+		docs = append(docs, bson.D{
+							{"contract", result.Receiver}, 
+							{"baseURI", "https://sevenseas.mypinata.cloud/ipfs/"},
+							{"tokenURI", result.MethodParams["tokenURI"]},
+							{"tokenId", result.TokenId},
+							{"wallets", result.Wallets},
+						})
 	}
-	fmt.Println("There are a total number of ", len(results), "LCUSD transactions.")
-	fmt.Println("The sender and event from are different: ", numSenderDiff)
-	fmt.Println("The receiver and event to are different: ", numReceiverDiff)
-	fmt.Println("The amount and event value do not tally: ", numValueDiff)
-	fmt.Println("The event.EventParams not equal to 1: ", eventSizeNotFulfill)
+
+	fmt.Println(len(docs))
+
+	// ******************************************************* //
+	// Step 5: InsertMany docs into the new collection "nfts" 
+	// ******************************************************* //
+	coll = client.Database("events").Collection("nfts")
+	result, err := coll.InsertMany(context.TODO(), docs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Number of documents inserted: %d\n", len(result.InsertedIDs))
 }
