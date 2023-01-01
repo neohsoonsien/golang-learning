1592d521e05065163cc6f981379e2c191d4ffa74
neohsoonsien <neohsbrother@gmail.com>
2023-01-01 13:24:23 +0800

    update(marshal): append json object into array, export to file and import back

--- marshal/marshal.go
+++ marshal/marshal.go
@@ -12,9 +12,19 @@ type User struct {
     Active 		bool	`json:"active"`
 }
 
+type Data struct {
+	Environment	string	`json:"environment"`
+	Value		[]User	`json:"value"`
+}
+
 func main() {
+	// define array to collect the json object
+	array := []User{}
+	array = append(array, User{"John", 29, true})
+	array = append(array, User{"James", 32, true})
+
 	// encode any type into a byte slice
-	value, err := json.Marshal(User{"John", 29, true})
+	value, err := json.Marshal(array)
 	if err != nil {
 		panic(err)
 	}
@@ -22,4 +32,19 @@ func main() {
 
 	// output to json file
 	_ = ioutil.WriteFile("test.json", value, 0644)
+
+	// read in from the json file
+	content, _ := ioutil.ReadFile("./test.json")
+
+	newArray := []*User{}
+	err = json.Unmarshal(content, &newArray)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(newArray); i++ {
+		fmt.Println(newArray[i].Name)
+		fmt.Println(newArray[i].Age)
+		fmt.Println(newArray[i].Active)
+	}
 }
