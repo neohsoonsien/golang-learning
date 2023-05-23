2a5c44809d207f4d09bfa14758f606692663f7c6
neohsoonsien <neohsbrother@gmail.com>
2023-05-23 22:04:17 +0800

    update(structs): revise declaring and initializing struct as pointer

--- structs/struct.go
+++ structs/struct.go
@@ -16,7 +16,12 @@ func newStudent(name string, age int) (*student) {
 }
 
 func Student() {
+
+	var pupil *student
+	pupil = &student{name: "Jimmy", age: 30}
+
 	fmt.Println(student{name: "John", age: 29})
 	fmt.Println(&student{name: "Steven", age: 25})
 	fmt.Println(newStudent("James", 30))
+	fmt.Println(pupil)
 }
