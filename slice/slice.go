52a9f7d50b996d06f6ec5f7b343253bf68f03017
neohsoonsien <neohsbrother@gmail.com>
2023-05-24 21:39:51 +0800

    update(slice): declare and initialize a slice

--- slice/slice.go
+++ slice/slice.go
@@ -22,6 +22,7 @@ func Slice() {
 	// Example 2: triple dots as argument / parameter in variadic function
 	slice = []int{1, 2, 3, 4, 5}
 	fmt.Println(sum(slice...))
+	fmt.Println(slice[1:3])
 
 	// Example 3: append all the trailing elements into a slice
 	slice2 := []int{1, 7, 3}
@@ -30,9 +31,15 @@ func Slice() {
 	// Example 4: pop and shift functions for the slice
 	var value int
 	value, shifted := shift(slice...)
-	fmt.Printf("The first element is %v, and the slice after shift operation is %v", value, shifted)
+	fmt.Printf("The first element is %v, and the slice after shift operation is %v\n", value, shifted)
 	value, popped := pop(slice...)
-	fmt.Printf("The last element is %v, and the slice after pop operation is %v", value, popped)
+	fmt.Printf("The last element is %v, and the slice after pop operation is %v\n", value, popped)
+
+	// Example 5: Initialize a slice
+	var intSlice = make([]int, 10)
+	intSlice[0] = 0
+	intSlice[9] = 9
+	fmt.Printf("The intSlice is %v\n", intSlice)
 }
 
 // Variadic function parameters
