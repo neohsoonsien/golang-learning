76823b2191c0d0868d0fa35587598d9bf3d55ba3
neohsoonsien <neohsbrother@gmail.com>
2023-06-20 21:56:08 +0800

    update(pointer): checked that a pointer to a declared variable will never be nil

--- pointer/string.go
+++ pointer/string.go
@@ -8,7 +8,19 @@ func getString(param *string) string {
 	return *param
 }
 
+func checkMemoryAddress(memory *string) string {
+
+	if memory == nil {
+		return "Empty memory"
+	}
+
+	return "Non-empty memory"
+}
+
 func String() {
 	value := "Hello"
 	fmt.Println(getString(&value))
+
+	var key string
+	fmt.Println(checkMemoryAddress(&key))
 }
