42fb3deb646f9cd557d8f0f646256857e463e26b
neohsoonsien <neohsbrother@gmail.com>
2023-09-24 14:29:32 +0800

    update(mapping): struct with map type

--- mapping/MapStudent.go
+++ mapping/MapStudent.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 )
 
-type Student struct {
-    Name 		string		`json:"name"`
-    Id			string		`json:"id"`
+type Teacher struct {
+	Name     string              `json:"Name"`
+	Subjects map[string][]string `json:"Subjects"`
 }
 
-func MapStudent(name string, id string) (map[string]*Student) {
+type Student struct {
+	Name string `json:"Name"`
+	Id   string `json:"Id"`
+}
+
+func MapStudent(name string, id string) map[string]*Student {
 	// create empty map
 	array := make(map[string]int)
 
-	// insert key/value 
+	// insert key/value
 	array["one"] = 1
 	array["two"] = 2
 	fmt.Println("array: ", array)
@@ -24,11 +29,26 @@ func MapStudent(name string, id string) (map[string]*Student) {
 
 	// return index of a key/value pairs with exist check
 	index, exist := array["one"]
-    fmt.Println("index: ", index, "value: ", exist)
+	fmt.Println("index: ", index, "value: ", exist)
 
 	// declare studentList
 	studentList := make(map[string]*Student, 0)
-	studentList[name] = &Student{name, id}
+	studentList[name] = &Student{
+		Name: name,
+		Id:   id,
+	}
+
+	// the object component is a map
+	classes := []string{"1H", "2B", "2A"}
+	subjectsList := make(map[string][]string)
+	subjectsList["History"] = classes
+	teacher := Teacher{
+		Name:     "Jenny",
+		Subjects: subjectsList,
+	}
+	for subject, classes := range teacher.Subjects {
+		fmt.Printf("Staff name: %v, subject taught: %v, classes: %v\n", teacher.Name, subject, classes)
+	}
 
 	return studentList
 }
