58d76117308af58b15ae7f2e3d7f5039c2cbc340
neohsoonsien <neohsbrother@gmail.com>
2024-02-22 22:08:37 +0800

    update(test): adding TestMain to the package

--- test/greet_test.go
+++ test/greet_test.go
@@ -1,11 +1,28 @@
 package test
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	log.Print("BEFORE the tests!")
+	exitVal := m.Run()
+	log.Print("AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
 func TestGreet(t *testing.T) {
-    name := "James"
-    fmt.Println("The function Hello returns ", Greet(name))
+	name := "James"
+	t.Logf("The function Hello returns %v", Greet(name))
+}
+
+func TestA(t *testing.T) {
+	t.Log("TestA running")
+}
+
+func TestB(t *testing.T) {
+	t.Log("TestB running")
 }
