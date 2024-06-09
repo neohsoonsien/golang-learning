276d88870281e8fde6c390398ae6d475fa0ce102
neohsoonsien <neohsbrother@gmail.com>
2024-06-09 12:47:26 +0800

    update(mongodb): modify the FindOne example, no debug yet

--- mongodb/FindOne.go
+++ mongodb/FindOne.go
@@ -3,25 +3,26 @@ package mongodb
 import (
 	"context"
 	"fmt"
-	"os"
-
-	godotenv "github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.uber.org/zap"
 )
 
-func FindOne() {
+type Student struct {
+	Id   primitive.ObjectID `bson:"_id"`
+	Name string             `bson:"name" json:"name"`
+	Age  int32              `bson:"age" json:"age"`
+}
+
+func FindOne(mongoURI string) {
 
 	// ******************************************************* //
-	// Step 1: obtain the mongoURI connection string from .env file
+	// Step 1: initialize logger
 	// ******************************************************* //
-	godotenv.Load()
-	mongoURI := os.Getenv("MONGODB_URI")
-
 	// initialize logger
 	logger := zap.NewExample().Sugar()
 	defer logger.Sync()
@@ -49,10 +50,10 @@ func FindOne() {
 	// Step 3: Query from the database
 	// ******************************************************* //
 	// query from the collection
-	collection := client.Database("tracker").Collection("coindex1_nft_price")
-	opts := options.FindOne().SetSort(bson.D{{"tran_price", 1}})
-	nftPrice := &Coindex1NftPrice{}
-	err = collection.FindOne(context.TODO(), bson.D{{"token_id", "388005071993967880"}}, opts).Decode(nftPrice)
+	collection := client.Database("students").Collection("students")
+	opts := options.FindOne().SetSort(bson.D{{"name", 1}})
+	student := &Student{}
+	err = collection.FindOne(context.TODO(), bson.D{{"name", "Jason"}}, opts).Decode(student)
 	if err != nil {
 		// ErrNoDocuments means that the filter did not match any documents in
 		// the collection.
@@ -61,5 +62,5 @@ func FindOne() {
 		}
 		logger.Error(err)
 	}
-	logger.Infof("The found NFT is %v", nftPrice)
+	logger.Infof("The student is %v", student)
 }
