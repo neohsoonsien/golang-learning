3c0c490eeca22d0ef335e3d62c3be1ea7b9d3ac8
neohsoonsien <neohsbrother@gmail.com>
2026-04-21 21:37:06 +0800

    update(mock): adding more methods to Student struct

--- mock/mock.go
+++ mock/mock.go
@@ -4,10 +4,28 @@ type Student struct {
 	Name string `json:"name"`
 	Id   string `json:"id"`
 }
+func (s *Student) SetName(name string) error {
+	s.Name = name
+
+	return nil
+}
 
 func (s *Student) GetName() (*string, error) {
 	if s != nil {
 		return &s.Name, nil
+	}
+	return nil, nil
+}
+
+func (s *Student) SetId(id string) error {
+	s.Id = id
+
+	return nil
+}
+
+func (s *Student) GetId() (*string, error) {
+	if s != nil {
+		return &s.Id, nil
 	}
 	return nil, nil
 }
--- mock/mock_test.go
+++ mock/mock_test.go
@@ -18,16 +18,52 @@ func (s *MockStudent) GetName() (*string, error) {
 	return nil, nil
 }
 
+func TestSetName(t *testing.T) {
+	t.Log("TestSetName begins")
+
+	student := Student{}
+
+	err := student.SetName("John")
+
+	assert.DeepEqual(t, err, nil)
+	assert.DeepEqual(t, student, Student{Name: "John"})
+}
+
 func TestGetName(t *testing.T) {
 	t.Log("TestGetName begins")
 
-	mockStudent := MockStudent{
+	student := Student{
 		Name: "Tim",
 		Id:   "A1234",
 	}
 
-	name, err := mockStudent.GetName()
+	name, err := student.GetName()
 
 	assert.DeepEqual(t, err, nil)
 	assert.DeepEqual(t, *name, "Tim")
+}
+
+func TestSetId(t *testing.T) {
+	t.Log("TestSetId begins")
+
+	student := Student{}
+
+	err := student.SetId("A1234")
+
+	assert.DeepEqual(t, err, nil)
+	assert.DeepEqual(t, student, Student{Id: "A1234"})
+}
+
+func TestGetId(t *testing.T) {
+	t.Log("TestGetId begins")
+
+	student := Student{
+		Name: "Tim",
+		Id:   "A1234",
+	}
+
+	id, err := student.GetId()
+
+	assert.DeepEqual(t, err, nil)
+	assert.DeepEqual(t, *id, "A1234")
 }
